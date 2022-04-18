be485f8331a861b9f768552bf12a8b2bd545dde4
PRODIP SARKAR <prodip1023@gmail.com>
2022-04-18 23:50:11 +0530

    folder name changed and update some theory

--- LANGUAGE FUNDAMENTALS/hello.py
+++
@@ -1,7 +0,0 @@
-print(" My name is prodip")
-
-
-# Addition  two numbers 
-a= 10
-b= 20
-print("sum of this two number is :",(a+b))
---
+++ LANGUAGE_FUNDAMENTALS/hello.py
@@ -0,0 +1,31 @@
+print(" My name is prodip")
+
+
+# Addition  two numbers 
+a= 10
+b= 20
+print("sum of this two number is :",(a+b))
+
+# reserved word
+
+
+import keyword
+print("the list of keywords:")
+print(keyword.kwlist)
+
+
+# type of variable
+a = 10 
+print(type(a))
+
+# id 
+print (id(a))
+
+# complex data type
+a = 10 + 15j
+print(a.real) 
+print(a.imag)
+# sum of complex data type 
+c = 10+3j
+d = 15+6j
+print(c+d)
