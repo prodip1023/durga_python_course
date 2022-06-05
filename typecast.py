5d1f2af74b3326f791a5247fb9d18c2c5869dadc
PRODIP SARKAR <prodip1023@gmail.com>
2022-06-05 13:46:46 +0530

    new file add

---
+++ typecast.py
@@ -0,0 +1,58 @@
+#int() typecasting 
+print(int(123.88))
+#print(int(1+4j)) error 
+print(int(True))
+print(int(False))
+print(int("10"))
+#print(int("10.44")) error 
+#print(int("0B1111")) error
+#print(int("tiger")) error 
+
+#float() typecasting
+print(float(10))
+#print(float(10+5j)) error
+print(float(True))
+print(float(False))
+print(float(10))
+#print(float("pro")) error
+print(float("10"))
+print(float("10.5"))
+#print(float("0B1111")) error
+
+#complex() typecasting
+print(complex(10))
+print(complex(10.5))
+print(complex(True))
+print(complex(False))
+print(complex("10"))
+print(complex("10.5"))
+#print(complex("india"))  error
+print(complex(10,3))
+print(complex(3,-3))
+print(complex(True,False))
+
+#bool() typecasting
+print(bool(1))
+print(bool(0))
+print(bool(10))
+print(bool(10.5))
+print(bool(10+2j))
+print(bool("you"))
+print(bool("True"))
+print(bool("False"))
+print(bool(""))
+
+
+#str() typecasting
+print(str(10))
+print(str(10.5))
+print(str(10+5j))
+print(str(True))
+
+
+
+
+
+
+
+
