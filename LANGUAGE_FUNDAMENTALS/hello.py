b6d1f93eb4507ae5af592500cf8ac134e82f8c81
PRODIP SARKAR <prodip1023@gmail.com>
2022-06-05 11:09:27 +0530

    add new code

--- LANGUAGE_FUNDAMENTALS/hello.py
+++ LANGUAGE_FUNDAMENTALS/hello.py
@@ -29,3 +29,22 @@ print(a.imag)
 c = 10+3j
 d = 15+6j
 print(c+d)
+
+# int data type 
+a= 10
+print(a)
+print(type(a))
+
+"""" represent int values in following forms :
+    1. Decimal form (base 10)(0-9)
+    2. Binary form (base 2) (0 & 1)(any base to -> bin)(0b or 0B)
+    3. Octal form (base 8)(0-7) (0o or 0O)(any base to -> oct)
+    4. Hexa decimal form (base 16)(0-9,a-f)(0x or 0X)(any base to -> hex)
+"""
+# Base conversion 
+print(bin(15))
+print(bin(0o11))
+print(oct(10))
+print(oct(0B1111))
+print(hex(100))
+print(hex(0B1111))
