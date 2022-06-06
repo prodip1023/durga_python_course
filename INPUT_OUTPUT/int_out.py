15a49738ae9f6079ffe7ebc6e12ab5896588eab6
PRODIP SARKAR <prodip1023@gmail.com>
2022-06-07 00:18:17 +0530

    start i/o section

---
+++ INPUT_OUTPUT/int_out.py
@@ -0,0 +1,14 @@
+#input 
+from curses import raw
+
+
+'''x = input("enter the first number :")
+print(type(x))'''
+
+
+#Read 2 numbers from the key board and print sum
+
+first_number = int(input("enter the first  number :"))
+second_number = int(input("enter the  second  number :"))
+print("sum of these two number is :",first_number+second_number)
+
